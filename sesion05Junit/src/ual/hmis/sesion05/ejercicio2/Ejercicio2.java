2a921d3aa7c14f05a4d1eb52b22bf1b37073e093
José Joaquín Cañadas Martínez <jjcanada@ual.es>
2021-04-19 20:58:45 +0200

    updated pom

--- sesion05Junit/src/ual/hmis/sesion05/ejercicio2/Ejercicio2.java
+++ sesion05Junit/src/ual/hmis/sesion05/ejercicio2/Ejercicio2.java
@@ -3,7 +3,7 @@ package ual.hmis.sesion05.ejercicio2;
 public class Ejercicio2 {
 	public boolean login (String username, String password){
 		// comprobar que sean distintos de vacio
-		if (username.isEmpty() || password.isEmpty())
+		if (username=="" || password.isEmpty())
 			return false;
 		// comprobar que la longitud sea < 30
 		if (username.length()>= 30 || password.length()>=30)
